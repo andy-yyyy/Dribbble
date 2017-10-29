9c5ebab9974d8f64934a576c025908eaba6b56ec
andy <andy821280045@gmail.com>
2017-10-29 16:49:03 +0800

    登录页面

--- app/src/main/java/com/andy/dribbble/LoginAct.java
+++ app/src/main/java/com/andy/dribbble/LoginAct.java
@@ -2,11 +2,19 @@ package com.andy.dribbble;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -35,6 +43,7 @@ public class LoginAct extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_login);
+        initView();
         Button login = (Button) findViewById(R.id.login);
         token = (TextView) findViewById(R.id.token);
         if (login != null) {
@@ -51,6 +60,14 @@ public class LoginAct extends BaseActivity {
                 }
             });
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -80,5 +97,23 @@ public class LoginAct extends BaseActivity {
             }
         });
         Log.d("TAG","code:"+code);
+    }
+
+    private void initView() {
+        // 沉浸式状态栏
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams lp = getWindow().getAttributes();
+            lp.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS|lp.flags);
+        }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        if (ab != null) {
+            ab.setDisplayHomeAsUpEnabled(true);
+            // 返回箭头改成白色
+            Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.abc_ic_ab_back_material);
+            upArrow.setColorFilter(getResources().getColor(R.color.white), PorterDuff.Mode.SRC_ATOP);
+            getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        }
     }
 }
